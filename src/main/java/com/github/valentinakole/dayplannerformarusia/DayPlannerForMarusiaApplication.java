05a3f388ebfa3f51e6fb75a7d0a06480b6c059c9
valentinakolesnikova <polikarpova-v@yandex.ru>
2023-08-16 15:44:43 +0300

    feat:

---
+++ src/main/java/com/github/valentinakole/dayplannerformarusia/DayPlannerForMarusiaApplication.java
@@ -0,0 +1,13 @@
+package com.github.valentinakole.dayplannerformarusia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DayPlannerForMarusiaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DayPlannerForMarusiaApplication.class, args);
+	}
+
+}
