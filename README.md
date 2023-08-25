f1323bee18d624920aa761f90a7642097aaa7f17
valentinakolesnikova <polikarpova-v@yandex.ru>
2023-08-25 21:44:58 +0300

    feat: добавить основные команды помощника

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Песочница
-https://day-planner-for-marusia-git-valentinakolesnikova-dev.apps.sandbox-m4.g2pi.p1.openshiftapps.com/swagger-ui/index.html
+https://week-planner-for-marusia-git-valentinakolesnikova-dev.apps.sandbox-m4.g2pi.p1.openshiftapps.com/swagger-ui/index.htmlindex.html
 
--- src/main/java/com/github/valentinakole/weekplannerformarusia/service/WeekPlannerServiceImpl.java
+++ src/main/java/com/github/valentinakole/weekplannerformarusia/service/WeekPlannerServiceImpl.java
@@ -7,6 +7,7 @@ import com.github.valentinakole.weekplannerformarusia.model.Response;
 import com.github.valentinakole.weekplannerformarusia.model.Session;
 import com.google.gson.Gson;
 import lombok.extern.slf4j.Slf4j;
+import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 import org.springframework.http.HttpHeaders;
@@ -43,23 +44,33 @@ public class WeekPlannerServiceImpl implements WeekPlannerService {
             String escapedPhrase = new JSONObject(jsonString)
                     .getJSONObject("request").getString("original_utterance")
                     .replace("\"", "").replace("\\\"", "");
+            String responsePhrase;
 
-//            JSONArray jsonArray = jsonObject.getJSONObject("state").getJSONObject("user").getJSONArray("data");
-            DayWeek[] dayWeeks = new DayWeek[7];
-            for (int i = 0; i < 7; i++) {
-//                JSONObject jsonObject1 = jsonArray.getJSONObject(i);
-//                dayWeeks[i] = DayWeek.builder().date(jsonObject1.getString("date"))
-//                        .timeTable(jsonArray.getJSONObject(i).getString("timeTable")).build();
-                dayWeeks[i] = DayWeek.builder().date("12.03.2012")
-                        .timeTable("12:00 Обед 16:00 Полдник").build();
+            switch (escapedPhrase) {
+                case "Расскажи план на неделю":
+                    responsePhrase = "Сейчас расскажу все ваши планы на неделю";
+                    break;
+                case "Добавь событие":
+                    responsePhrase = "На какой день добавить событие?";
+                    break;
+                case "Расскажи план на сегодня":
+                    responsePhrase = "Сегодня нас ждет много интересного";
+                    break;
+                case "Расскажи план на завтра":
+                    responsePhrase = "Завтра будет план точно";
+                    break;
+                case "Справка":
+                    responsePhrase = "Доступные команды: Добавить событие , Расскажи план на сегодня , Расскажи план на завтра , Расскажи план на неделю";
+                    break;
+                default:
+                    responsePhrase = "Выбрана незвестная команда";
             }
 
-            dayWeeks[0].setTimeTable(dayWeeks[0].getTimeTable() + escapedPhrase);
+            DayWeek[] dayWeeks = getDayWeeks(jsonObject);
             marusiaResponse = MarusiaResponse.builder()
                     .response(Response.builder()
-                            .text(escapedPhrase)
-                            .tts(String.format(String.format(escapedPhrase + " текущее время " +
-                                    new SimpleDateFormat("HH:mm:ss").format(System.currentTimeMillis()))))
+                            .text(responsePhrase)
+                            .tts(responsePhrase)
                             .end_session(false).build())
                     .session(Session.builder()
                             .user_id(jsonObject.getJSONObject("session").getString("user_id"))
@@ -73,5 +84,26 @@ public class WeekPlannerServiceImpl implements WeekPlannerService {
             return null;
         }
         return marusiaResponse;
+    }
+
+    private DayWeek[] getDayWeeks(JSONObject jsonObject) {
+        DayWeek[] dayWeeks = new DayWeek[7];
+        try {
+            JSONArray jsonArray = jsonObject.getJSONObject("state").getJSONObject("user").getJSONArray("data");
+            for (int i = 0; i < 7; i++) {
+                JSONObject jsonObject1 = jsonArray.getJSONObject(i);
+                dayWeeks[i] = DayWeek.builder().date(jsonObject1.getString("date"))
+                        .timeTable(jsonArray.getJSONObject(i).getString("timeTable")).build();
+            }
+        } catch (JSONException e) {
+            log.info("Не удалось получить из запроса данные планировщика, ошибка {}", e.getMessage());
+            log.info("Выполняем заполнение планировщика данными");
+            for (int i = 0; i < 7; i++) {
+                dayWeeks[i] = DayWeek.builder().date(new SimpleDateFormat("dd-MM-yyyy")
+                                .format(System.currentTimeMillis() + i * 86400000))
+                        .timeTable("").build();
+            }
+        }
+        return dayWeeks;
     }
 }
