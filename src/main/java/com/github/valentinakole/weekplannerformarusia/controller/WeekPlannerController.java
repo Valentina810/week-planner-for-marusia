be96ebfc33a0bdc26fbca0513cad14b18172f442
valentinakolesnikova <polikarpova-v@yandex.ru>
2023-08-25 17:53:01 +0300

    feat: исправлен WeekPlannerController

---
+++ src/main/java/com/github/valentinakole/weekplannerformarusia/controller/WeekPlannerController.java
@@ -0,0 +1,22 @@
+package com.github.valentinakole.weekplannerformarusia.controller;
+
+import com.github.valentinakole.weekplannerformarusia.service.WeekPlannerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class WeekPlannerController {
+    private final WeekPlannerService weekPlannerService;
+
+    @PostMapping("/webhook")
+    @ResponseStatus(HttpStatus.OK)
+    public ResponseEntity createStat(@RequestBody Object object) {
+        return weekPlannerService.getResponse(object);
+    }
+}
