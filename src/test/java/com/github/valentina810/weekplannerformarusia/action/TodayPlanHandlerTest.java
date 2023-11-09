5c1226a45bacd4910693d6602d35a246797d9741
valentinakolesnikova <polikarpova-v@yandex.ru>
2023-11-09 20:40:36 +0300

    fix: добавить логирование в TodayPlanHandlerTest

--- src/test/java/com/github/valentina810/weekplannerformarusia/action/TodayPlanHandlerTest.java
+++ src/test/java/com/github/valentina810/weekplannerformarusia/action/TodayPlanHandlerTest.java
@@ -4,6 +4,8 @@ import com.github.valentina810.weekplannerformarusia.action.parameterized.todayp
 import com.github.valentina810.weekplannerformarusia.model.request.UserRequest;
 import com.github.valentina810.weekplannerformarusia.util.FileReader;
 import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,10 +13,13 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@Slf4j
 @SpringBootTest
 public class TodayPlanHandlerTest {
     @Autowired
     private ActionExecutor actionExecutor;
+    @Autowired
+    private UserRequest userRequest;
 
     @ParameterizedTest
     @MethodSource("com.github.valentina810.weekplannerformarusia.action.parameterized.todayplan.TodayPlanTestData#providerTodayPlanHandlerTest")
@@ -23,7 +28,9 @@ public class TodayPlanHandlerTest {
                 .replace("todayDate", parameterForTodayPlanTest.getTodayDate())
                 .replace("todayEvents", parameterForTodayPlanTest.getTodayEvents());
 
-        UserRequest userRequest = new Gson().fromJson(json, UserRequest.class);
+        log.info("Из файла получен json {}", json);
+        userRequest = new Gson().fromJson(json, UserRequest.class);
+        log.info("userRequest = {}", new Gson().toJson(userRequest, JsonElement.class));
         actionExecutor.createUserResponse(userRequest);
 
         assertEquals(parameterForTodayPlanTest.getExpectedResult(),
