79203f94119ff75ce2cc5c2a5aa7535e3e20fae2
valentinakolesnikova <polikarpova-v@yandex.ru>
2023-11-30 20:31:46 +0300

    feat: добавить тесты для ExitHandler

---
+++ src/test/java/com/github/valentina810/weekplannerformarusia/action/ExitHandlerTest.java
@@ -0,0 +1,38 @@
+package com.github.valentina810.weekplannerformarusia.action;
+
+import com.github.valentina810.weekplannerformarusia.model.request.UserRequest;
+import com.github.valentina810.weekplannerformarusia.util.FileReader;
+import com.google.gson.Gson;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+public class ExitHandlerTest {
+    @Autowired
+    private ActionExecutor actionExecutor;
+
+    @Test
+    public void getGoodbye_whenExitCommand_thenReturnMessageGoodbyeAndEndSession() {
+        String json = FileReader.loadStringFromFile("action/plantodate/PlanEmpty.json")
+                .replace("testDate", LocalDate.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy")))
+                .replace("testEvents", "")
+                .replace("phrase", "пока");
+
+        UserRequest userRequest = new Gson().fromJson(json, UserRequest.class);
+        actionExecutor.createUserResponse(userRequest);
+
+        assertAll(
+                () -> assertEquals("До свидания!",
+                        actionExecutor.getUserResponse().getResponse().getText()),
+                () -> assertTrue(actionExecutor.getUserResponse().getResponse().isEnd_session())
+        );
+    }
+}
