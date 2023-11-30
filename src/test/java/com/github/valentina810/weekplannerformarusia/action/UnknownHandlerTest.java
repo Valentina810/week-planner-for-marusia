b42fb474ff147303d265062e67bdb52cdc289892
valentinakolesnikova <polikarpova-v@yandex.ru>
2023-11-30 18:39:44 +0300

    feat: добавить тесты для UnknownHandler

---
+++ src/test/java/com/github/valentina810/weekplannerformarusia/action/UnknownHandlerTest.java
@@ -0,0 +1,55 @@
+package com.github.valentina810.weekplannerformarusia.action;
+
+import com.github.valentina810.weekplannerformarusia.model.request.UserRequest;
+import com.github.valentina810.weekplannerformarusia.util.FileReader;
+import com.google.gson.Gson;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+
+@SpringBootTest
+public class UnknownHandlerTest {
+    @Autowired
+    private ActionExecutor actionExecutor;
+
+    @Test
+    public void getPhraseUnknownCommand_whenUnknownCommand_thenReturnMessage() {
+        String json = FileReader.loadStringFromFile("action/plantodate/PlanEmpty.json")
+                .replace("testDate", LocalDate.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy")))
+                .replace("testEvents", "")
+                .replace("phrase", "PhraseUnknown");
+
+        UserRequest userRequest = new Gson().fromJson(json, UserRequest.class);
+        actionExecutor.createUserResponse(userRequest);
+
+        assertAll(
+                () -> assertEquals("Получена неизвестная команда",
+                        actionExecutor.getUserResponse().getResponse().getText()),
+                () -> assertFalse(actionExecutor.getUserResponse().getResponse().isEnd_session())
+        );
+    }
+
+    @Test
+    public void getPhraseUnknownCommand_whenCommandEmpty_thenReturnMessage() {
+        String json = FileReader.loadStringFromFile("action/plantodate/PlanEmpty.json")
+                .replace("testDate", LocalDate.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy")))
+                .replace("testEvents", "")
+                .replace("phrase", "");
+
+        UserRequest userRequest = new Gson().fromJson(json, UserRequest.class);
+        actionExecutor.createUserResponse(userRequest);
+
+        assertAll(
+                () -> assertEquals("Получена неизвестная команда",
+                        actionExecutor.getUserResponse().getResponse().getText()),
+                () -> assertFalse(actionExecutor.getUserResponse().getResponse().isEnd_session())
+        );
+    }
+}
