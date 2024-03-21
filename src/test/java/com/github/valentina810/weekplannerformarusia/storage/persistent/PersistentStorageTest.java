3109d1649b73976f4e4c7d20259de990c2a632ff
valentinakolesnikova <polikarpova-v@yandex.ru>
2024-03-21 22:20:28 +0300

    test: исправлены тесты для  PersistentStorage: убрано дублирование, добавлен тест с некорректным временем

--- src/test/java/com/github/valentina810/weekplannerformarusia/storage/persistent/PersistentStorageTest.java
+++ src/test/java/com/github/valentina810/weekplannerformarusia/storage/persistent/PersistentStorageTest.java
@@ -2,12 +2,14 @@ package com.github.valentina810.weekplannerformarusia.storage.persistent;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonSyntaxException;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.MockitoAnnotations;
+import org.junit.jupiter.api.TestInstance;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -16,29 +18,39 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.MockitoAnnotations.*;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class PersistentStorageTest {
 
     private PersistentStorage persistentStorage;
 
+    private Map<String, List<Event>> days;
+    private Week week;
     private static final String EVENT_DATE = LocalDate.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy"));
     private static final String EVENT_TIME_FOR_STRING = "одиннадцать часов тридцать восемь минут";
     private static final String EVENT_TIME_FOR_TIME = "11:38";
     private static final String EVENT_NAME = "Ужин";
 
+    private static final Event event = Event.builder().time(EVENT_TIME_FOR_TIME)
+            .name(EVENT_NAME)
+            .build();
+
+    @BeforeAll
+    void allSetup() {
+        days = new HashMap<>();
+        days.put(EVENT_DATE, List.of(event));
+        week = new Week(days);
+    }
+
     @BeforeEach
     void setUp() {
-        openMocks(this);
         persistentStorage = new PersistentStorage();
     }
 
     @Test
-    void setWeekStorage_whenObjectWithWeek_thenPersistentStorageSetWeekStorage() {
-        Week week = Week.builder().build();
-
-        week.addEvent(EVENT_DATE, new Event(EVENT_TIME_FOR_TIME, EVENT_NAME));
+    void setWeekStorage_shouldUpdateWeekStorage_thenProvidedWithValidWeekObject() {
         String json = "{\"week\":" + new Gson().toJson(week) + "}";
+
         persistentStorage.setWeekStorage(json);
 
         assertNotNull(persistentStorage.getWeekStorage());
@@ -56,9 +68,6 @@ public class PersistentStorageTest {
 
     @Test
     void getEventsByDay_whenExistingDay_thenReturnEvents() {
-        Week week = Week.builder().build();
-        Event event = new Event(EVENT_TIME_FOR_TIME, EVENT_NAME);
-        week.addEvent(EVENT_DATE, event);
         persistentStorage.setWeekStorage(week);
 
         List<Event> events = persistentStorage.getEventsByDay(EVENT_DATE);
@@ -67,7 +76,7 @@ public class PersistentStorageTest {
     }
 
     @Test
-    void getEventsByDay_whenNonExistingDay_thenReturnEmptyList() {
+    void getEventsByDays_whenWeekStorageEmpty_thenReturnEmptyList() {
         persistentStorage.setWeekStorage(Week.builder().build());
 
         List<Event> events = persistentStorage.getEventsByDay(EVENT_DATE);
@@ -76,10 +85,7 @@ public class PersistentStorageTest {
     }
 
     @Test
-    void getEventsByWeek_whenSetWeekStorage_thenReturnEventsByWeek() {
-        Week week = Week.builder().build();
-        Event event = new Event(EVENT_TIME_FOR_TIME, EVENT_NAME);
-        week.addEvent(EVENT_DATE, event);
+    void getEventsWeek_whenEventsInWeekStorage_thenReturnEventsForWeek() {
         persistentStorage.setWeekStorage(week);
 
         Map<String, List<Event>> eventsByWeek = persistentStorage.getEventsByWeek();
@@ -90,17 +96,29 @@ public class PersistentStorageTest {
     }
 
     @Test
+    void addEventWithIncorrectTime_shouldBeaAddedEvent_thenReturnListOfEvents() {
+        event.setTime("00:00");
+        persistentStorage.addEvent(EVENT_DATE, "incorrect", EVENT_NAME);
+
+        Map<String, List<Event>> eventsByWeek = persistentStorage.getEventsByWeek();
+
+        assertTrue(eventsByWeek.containsKey(EVENT_DATE));
+        assertEquals(event, eventsByWeek.get(EVENT_DATE).get(0));
+    }
+
+    @Test
     void setWeekStorage_whenInvalidJson_thenNotSetWeekStorage() {
         String invalidJson = "{invalidJson}";
+
         assertThrows(JsonSyntaxException.class, () -> persistentStorage.setWeekStorage(invalidJson));
+        assertNull(persistentStorage.getWeekStorage());
     }
 
     @Test
     void addEvent_whenWeekStorageNotInitialization_thenInitializeWeekStorageAndAddEvent() {
-        PersistentStorage storage = new PersistentStorage();
-
-        String event = storage.addEvent(EVENT_DATE, EVENT_TIME_FOR_STRING, EVENT_NAME);
+        String event = persistentStorage.addEvent(EVENT_DATE, EVENT_TIME_FOR_STRING, EVENT_NAME);
 
         assertEquals(EVENT_DATE + " " + EVENT_TIME_FOR_TIME + " " + EVENT_NAME, event);
+        assertNotNull(persistentStorage);
     }
 }
