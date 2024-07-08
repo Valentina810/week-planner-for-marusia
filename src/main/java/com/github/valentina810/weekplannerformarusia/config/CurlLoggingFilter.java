b5ca71bcbc42a6bda602525c1ed6ff81f7562445
valentinakolesnikova <valentinavasileva34@gmail.com>
2024-07-09 00:32:44 +0300

    feat: добавить фильтрацию входящих запросов: обрабатывается только POST с заголовком 'user-agent: MailRu-VC/1.0'

--- src/main/java/com/github/valentina810/weekplannerformarusia/config/CurlLoggingFilter.java
+++ src/main/java/com/github/valentina810/weekplannerformarusia/config/CurlLoggingFilter.java
@@ -14,22 +14,50 @@ import org.springframework.web.util.ContentCachingRequestWrapper;
 import org.springframework.web.util.ContentCachingResponseWrapper;
 
 import java.io.IOException;
+import java.util.function.Predicate;
+
+import static jakarta.servlet.http.HttpServletResponse.SC_METHOD_NOT_ALLOWED;
+import static java.util.Optional.of;
 
 @Slf4j
 public class CurlLoggingFilter implements Filter {
 
+    private static final String USER_AGENT_HEADER = "User-Agent";
+    private static final String ALLOWED_USER_AGENT = "MailRu-VC/1.0";
+
     @Override
-    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
-            throws IOException, ServletException {
-        ContentCachingRequestWrapper cachedRequest = new ContentCachingRequestWrapper((HttpServletRequest) servletRequest);
-        ContentCachingResponseWrapper cachedResponse = new ContentCachingResponseWrapper((HttpServletResponse) servletResponse);
-        filterChain.doFilter(cachedRequest, cachedResponse);
-        logRequestResponseAsCurl(cachedRequest, cachedResponse);
-        cachedResponse.copyBodyToResponse();
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) {
+        HttpServletRequest httpRequest = (HttpServletRequest) servletRequest;
+        HttpServletResponse httpResponse = (HttpServletResponse) servletResponse;
+        log.info(CurlCommandBuilder.buildCurlCommandRequest(new ContentCachingRequestWrapper(httpRequest)));
+
+        Predicate<HttpServletRequest> isPostMethod = req -> "POST".equalsIgnoreCase(req.getMethod());
+        Predicate<HttpServletRequest> hasAllowedUserAgent = req -> ALLOWED_USER_AGENT.equals(req.getHeader(USER_AGENT_HEADER));
+
+        of(httpRequest)
+                .filter(isPostMethod.and(hasAllowedUserAgent))
+                .ifPresentOrElse(req ->
+                                processSuccessfully(filterChain, req, httpResponse),
+                        () -> returnErrorMethodNotSupported(httpResponse));
     }
 
-    private void logRequestResponseAsCurl(ContentCachingRequestWrapper request, ContentCachingResponseWrapper response) {
-        log.info(CurlCommandBuilder.buildCurlCommandRequest(request));
-        log.info(CurlCommandBuilder.buildCurlCommandResponse(response));
+    private static void returnErrorMethodNotSupported(HttpServletResponse httpResponse) {
+        try {
+            httpResponse.sendError(SC_METHOD_NOT_ALLOWED, "Method Not Allowed");
+        } catch (IOException e) {
+            log.error("Возникла ошибка при отправке сообщения о неподдерживаемом методе: ", e);
+        }
+    }
+
+    private void processSuccessfully(FilterChain filterChain, HttpServletRequest req, HttpServletResponse httpResponse) {
+        try {
+            ContentCachingRequestWrapper cachedRequest = new ContentCachingRequestWrapper(req);
+            ContentCachingResponseWrapper cachedResponse = new ContentCachingResponseWrapper(httpResponse);
+            filterChain.doFilter(cachedRequest, cachedResponse);
+            log.info(CurlCommandBuilder.buildCurlCommandResponse(cachedResponse));
+            cachedResponse.copyBodyToResponse();
+        } catch (IOException | ServletException e) {
+            log.error("Возникла ошибка при логировании curl запроса/ответа: ", e);
+        }
     }
 }
