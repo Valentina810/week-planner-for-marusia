4851d28e6b2b55d74949a5ae45658cbf267ee8ef
valentinakolesnikova <valentinavasileva34@gmail.com>
2024-07-08 23:30:24 +0300

    feat: добавить логирование ответа

--- src/main/java/com/github/valentina810/weekplannerformarusia/config/CurlLoggingFilter.java
+++ src/main/java/com/github/valentina810/weekplannerformarusia/config/CurlLoggingFilter.java
@@ -1,7 +1,6 @@
 package com.github.valentina810.weekplannerformarusia.config;
 
 
-import com.github.valentina810.weekplannerformarusia.util.CachedBodyHttpServletRequest;
 import com.github.valentina810.weekplannerformarusia.util.CurlCommandBuilder;
 import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
@@ -9,59 +8,28 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.util.ContentCachingRequestWrapper;
+import org.springframework.web.util.ContentCachingResponseWrapper;
 
 import java.io.IOException;
-import java.util.Optional;
 
 @Slf4j
 public class CurlLoggingFilter implements Filter {
 
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) {
-        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
-        Optional<CachedBodyHttpServletRequest> cachedBodyHttpServletRequest =
-                getCachedBodyHttpServletRequest(httpServletRequest)
-                        .or(() -> handleIOExceptionDuringCreation(request, response, chain));
-        cachedBodyHttpServletRequest.ifPresentOrElse(
-                cachedRequest -> {
-                    log.info("------------------------------------------------------------------------------------------------------");
-                    log.info("Получен входящий запрос");
-                    log.info(CurlCommandBuilder.buildCurlCommand(cachedRequest));
-                    handleFilterChain(response, chain, cachedRequest);
-                },
-                () -> log.warn("Не удалось получить CachedBodyHttpServletRequest для запроса")
-        );
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
+            throws IOException, ServletException {
+        ContentCachingRequestWrapper cachedRequest = new ContentCachingRequestWrapper((HttpServletRequest) servletRequest);
+        ContentCachingResponseWrapper cachedResponse = new ContentCachingResponseWrapper((HttpServletResponse) servletResponse);
+        filterChain.doFilter(cachedRequest, cachedResponse);
+        logRequestResponseAsCurl(cachedRequest, cachedResponse);
+        cachedResponse.copyBodyToResponse();
     }
 
-    private Optional<CachedBodyHttpServletRequest> getCachedBodyHttpServletRequest(HttpServletRequest request) {
-        try {
-            return Optional.of(new CachedBodyHttpServletRequest(request));
-        } catch (IOException e) {
-            log.error("Ошибка при создании CachedBodyHttpServletRequest: ", e);
-            return Optional.empty();
-        }
-    }
-
-    private Optional<CachedBodyHttpServletRequest> handleIOExceptionDuringCreation(ServletRequest request, ServletResponse response, FilterChain chain) {
-        try {
-            chain.doFilter(request, response);
-        } catch (IOException | ServletException e) {
-            log.error("Ошибка при выполнении фильтрации запроса: ", e);
-        }
-        return Optional.empty();
-    }
-
-    private void handleFilterChain(ServletResponse response, FilterChain chain, CachedBodyHttpServletRequest cachedRequest) {
-        try {
-            chain.doFilter(cachedRequest, response);
-        } catch (IOException | ServletException e) {
-            log.error("Ошибка при выполнении фильтрации запроса: ", e);
-            if (e instanceof IOException) {
-                throw new RuntimeException(e);
-            } else if (e instanceof ServletException) {
-                throw new RuntimeException(e);
-            }
-        }
+    private void logRequestResponseAsCurl(ContentCachingRequestWrapper request, ContentCachingResponseWrapper response) {
+        log.info(CurlCommandBuilder.buildCurlCommandRequest(request));
+        log.info(CurlCommandBuilder.buildCurlCommandResponse(response));
     }
 }
