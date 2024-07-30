b03e3d0317536b0b136307be72278230864c73e9
valentinakolesnikova <valentinavasileva34@gmail.com>
2024-07-30 23:26:41 +0300

    feat: добавить черновой вариант логирования всех входящих запросов

--- src/main/java/com/github/valentina810/weekplannerformarusia/config/CurlLoggingFilter.java
+++ src/main/java/com/github/valentina810/weekplannerformarusia/config/CurlLoggingFilter.java
@@ -1,5 +1,7 @@
 package com.github.valentina810.weekplannerformarusia.config;
 
+import com.github.valentina810.weekplannerformarusia.util.CachedBodyHttpServletRequest;
+import com.github.valentina810.weekplannerformarusia.util.CurlCommandBuilder;
 import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -10,6 +12,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
+import java.util.Optional;
 import java.util.function.BiConsumer;
 import java.util.function.Consumer;
 
@@ -38,14 +41,32 @@ public class CurlLoggingFilter implements Filter {
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) {
         if (servletRequest instanceof HttpServletRequest request && servletResponse instanceof HttpServletResponse response) {
+
+            Optional<CachedBodyHttpServletRequest> cachedBodyHttpServletRequest =
+                    getCachedBodyHttpServletRequest(request)
+                            .or(Optional::empty);
+
+            cachedBodyHttpServletRequest.ifPresentOrElse(
+                    cachedRequest -> log.info(CurlCommandBuilder.buildCurlCommand(cachedRequest)),
+                    Optional::empty);
+
             processRequest.accept(request, response);
             if (!response.isCommitted()) {
                 try {
-                    filterChain.doFilter(request, response);
+                    filterChain.doFilter(cachedBodyHttpServletRequest.get(), response);
                 } catch (IOException | ServletException e) {
                     LOG_ERROR.accept(e.getMessage());
                 }
             }
+        }
+    }
+
+    private Optional<CachedBodyHttpServletRequest> getCachedBodyHttpServletRequest(HttpServletRequest request) {
+        try {
+            return Optional.of(new CachedBodyHttpServletRequest(request));
+        } catch (IOException e) {
+            log.error("Ошибка при создании CachedBodyHttpServletRequest: ", e);
+            return Optional.empty();
         }
     }
 }
---
+++ src/main/java/com/github/valentina810/weekplannerformarusia/util/CurlCommandBuilder.java
@@ -0,0 +1,58 @@
+package com.github.valentina810.weekplannerformarusia.util;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+
+import static java.lang.System.lineSeparator;
+import static java.util.Collections.list;
+import static java.util.stream.Collectors.joining;
+
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+public final class CurlCommandBuilder {
+
+    public static String buildCurlCommand(HttpServletRequest request) {
+        return new StringBuilder()
+                .append("curl -X ")
+                .append(request.getMethod())
+                .append(" '")
+                .append(request.getRequestURL())
+                .append(getQueryParams(request))
+                .append(getHeaders(request))
+                .append(getCurlBody(request))
+                .append(" # IP: ")
+                .append(request.getRemoteAddr())
+                .toString();
+    }
+
+    private static String getCurlBody(HttpServletRequest request) {
+        StringBuilder curlBody = new StringBuilder();
+        String data = " --data '";
+        try (BufferedReader reader = request.getReader()) {
+            String body = reader.lines().collect(joining(lineSeparator()));
+            if (!body.isEmpty()) {
+                curlBody.append(data).append(body).append("'");
+            }
+        } catch (IOException e) {
+            curlBody.append(data).append("Не удалось получить тело запроса").append("'");
+        }
+        return curlBody.toString();
+    }
+
+    private static String getHeaders(HttpServletRequest request) {
+        return list(request.getHeaderNames())
+                .stream()
+                .flatMap(headerName -> list(request.getHeaders(headerName))
+                        .stream()
+                        .map(headerValue -> " -H '" + headerName + ": " + headerValue + "'"))
+                .collect(joining(" "));
+    }
+
+    private static String getQueryParams(HttpServletRequest request) {
+        String queryString = request.getQueryString();
+        return (queryString != null) ? "?" + queryString + "'" : "'";
+    }
+}
