0460196ac3d62c937cb0265494588afd51b691fc
Valentina Kolesnikova <83814517+Valentina810@users.noreply.github.com>
2024-09-05 00:20:59 +0300

    Добавить фильтр входящих запросов по заголовку 'user-agent: MailRu-VC/1.0' (#97)
    * feat: добавлена информация по основным командам
    
    * docs: добавить описание команд, диаграмму для действия добавления события
    
    * docs: добавить ссылку на документацию в README.md
    
    * feat: добавить фильтрацию входящих запросов: обрабатывается только POST с заголовком 'user-agent: MailRu-VC/1.0'
    
    * feat: добавить черновой вариант логирования всех входящих запросов
    
    * feat: при логировании запроса в curl формате переключиться на использование внешней библиотеки, убрать лишний код из проекта
    
    * feat: обновить pom.xml
    
    * feat: тестовый коммит
    
    * feat: добавить в Dockerfile команды для включения зависимостей с github packages, поднять версию java до 21
    
    * feat: обновить Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,13 @@
-FROM maven:3.8.4-openjdk-17 as builder
+FROM jelastic/maven:3.9.5-openjdk-21 as builder
 WORKDIR /app
 COPY . /app/.
+COPY settings.xml /root/.m2/settings.xml
+ARG GITHUB_USERNAME
+ARG GITHUB_TOKEN
+RUN echo "machine github.com login $GITHUB_USERNAME password $GITHUB_TOKEN" > ~/.netrc
 RUN mvn -f /app/pom.xml clean package
 
-FROM eclipse-temurin:17-jre-alpine
+FROM eclipse-temurin:21-jre-alpine
 WORKDIR /app
 COPY --from=builder /app/target/*.jar /app/*.jar
 COPY tokens.json /app
--- src/main/java/com/github/valentina810/weekplannerformarusia/config/CurlLoggingFilter.java
+++ src/main/java/com/github/valentina810/weekplannerformarusia/config/CurlLoggingFilter.java
@@ -1,67 +1,57 @@
 package com.github.valentina810.weekplannerformarusia.config;
 
 
-import com.github.valentina810.weekplannerformarusia.util.CachedBodyHttpServletRequest;
-import com.github.valentina810.weekplannerformarusia.util.CurlCommandBuilder;
+import com.github.valentina810.Converter;
+import com.github.valentina810.utils.Data;
 import jakarta.servlet.Filter;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.ServletRequest;
 import jakarta.servlet.ServletResponse;
 import jakarta.servlet.http.HttpServletRequest;
+
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
-import java.util.Optional;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
+
+import static jakarta.servlet.http.HttpServletResponse.SC_UNSUPPORTED_MEDIA_TYPE;
 
 @Slf4j
 public class CurlLoggingFilter implements Filter {
 
+    private static final String REQUIRED_USER_AGENT = "MailRu-VC/1.0";
+    private static final String USER_AGENT_HEADER = "User-Agent";
+    private static final String SUPPORTED_METHOD = "POST";
+    private static final Consumer<String> LOG_ERROR = e -> log.error("Возникла ошибка при обработке запроса: {}", e);
+    private static final BiConsumer<HttpServletRequest, HttpServletResponse> processRequest =
+            (request, response) -> {
+                if (SUPPORTED_METHOD.equalsIgnoreCase(request.getMethod())) {
+                    if (!REQUIRED_USER_AGENT.equals(request.getHeader(USER_AGENT_HEADER))) {
+                        try {
+                            response.sendError(SC_UNSUPPORTED_MEDIA_TYPE);
+                        } catch (IOException e) {
+                            LOG_ERROR.accept(e.getMessage());
+                        }
+                    }
+                }
+            };
+
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) {
-        HttpServletRequest httpServletRequest = (HttpServletRequest) request;
-        Optional<CachedBodyHttpServletRequest> cachedBodyHttpServletRequest =
-                getCachedBodyHttpServletRequest(httpServletRequest)
-                        .or(() -> handleIOExceptionDuringCreation(request, response, chain));
-        cachedBodyHttpServletRequest.ifPresentOrElse(
-                cachedRequest -> {
-                    log.info("------------------------------------------------------------------------------------------------------");
-                    log.info("Получен входящий запрос");
-                    log.info(CurlCommandBuilder.buildCurlCommand(cachedRequest));
-                    handleFilterChain(response, chain, cachedRequest);
-                },
-                () -> log.warn("Не удалось получить CachedBodyHttpServletRequest для запроса")
-        );
-    }
-
-    private Optional<CachedBodyHttpServletRequest> getCachedBodyHttpServletRequest(HttpServletRequest request) {
-        try {
-            return Optional.of(new CachedBodyHttpServletRequest(request));
-        } catch (IOException e) {
-            log.error("Ошибка при создании CachedBodyHttpServletRequest: ", e);
-            return Optional.empty();
-        }
-    }
-
-    private Optional<CachedBodyHttpServletRequest> handleIOExceptionDuringCreation(ServletRequest request, ServletResponse response, FilterChain chain) {
-        try {
-            chain.doFilter(request, response);
-        } catch (IOException | ServletException e) {
-            log.error("Ошибка при выполнении фильтрации запроса: ", e);
-        }
-        return Optional.empty();
-    }
-
-    private void handleFilterChain(ServletResponse response, FilterChain chain, CachedBodyHttpServletRequest cachedRequest) {
-        try {
-            chain.doFilter(cachedRequest, response);
-        } catch (IOException | ServletException e) {
-            log.error("Ошибка при выполнении фильтрации запроса: ", e);
-            if (e instanceof IOException) {
-                throw new RuntimeException(e);
-            } else if (e instanceof ServletException) {
-                throw new RuntimeException(e);
-            }
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) {
+        if (servletRequest instanceof HttpServletRequest request && servletResponse instanceof HttpServletResponse response) {
+            Data convert = new Converter().convert(request);
+            log.info("Входящий запрос {}", convert.getCurl());
+            processRequest.accept(request, response);
+            if (!response.isCommitted()) {
+                try {
+                    filterChain.doFilter(convert.getCachedBodyHttpServletRequest(), response);
+                } catch (IOException | ServletException e) {
+                    LOG_ERROR.accept(e.getMessage());
+                }
+            } else log.info("Запрос не был обработан по причине отсутствия заголовка User-Agent с требуемым значением");
         }
     }
 }
