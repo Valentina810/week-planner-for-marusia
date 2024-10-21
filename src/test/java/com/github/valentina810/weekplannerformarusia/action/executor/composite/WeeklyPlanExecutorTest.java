08eade991b9cc6aa9664368762ccb3ee9daaccb4
valentinakolesnikova <valentinavasileva34@gmail.com>
2024-10-22 00:01:37 +0300

    feat: Добавить тесты для проверки удаления устраревших событий для разных таймзон

--- src/test/java/com/github/valentina810/weekplannerformarusia/action/executor/composite/WeeklyPlanExecutorTest.java
+++ src/test/java/com/github/valentina810/weekplannerformarusia/action/executor/composite/WeeklyPlanExecutorTest.java
@@ -6,10 +6,13 @@ import org.json.JSONObject;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.List;
+
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class WeeklyPlanExecutorTest extends BaseTest {
 
@@ -38,6 +41,22 @@ public class WeeklyPlanExecutorTest extends BaseTest {
 
         assertAll(
                 () -> assertEquals(parameterForWeeklyPlanTest.getExpectedResult(), objectResponse.getString("text")),
+                () -> assertFalse(objectResponse.getBoolean("end_session")),
+                () -> assertNull(getValue.apply(response, "session_state"))
+        );
+    }
+
+    @SneakyThrows
+    @ParameterizedTest
+    @MethodSource("com.github.valentina810.weekplannerformarusia.action.executor.composite.parameterized.weeklyplan.WeeklyPlanTestData#providerTimeZoneTest")
+    public void checkTheDeletionOfObsoleteEventsInTheWeeklyPlanWithTimeZone(List<String> days, String requestBody) {
+        JSONObject response = getResponse.apply(requestBody);
+        JSONObject objectResponse = getObjectResponse.apply(response);
+        String responsePhrase=objectResponse.getString("text");
+
+        assertAll(
+                () -> assertTrue(responsePhrase.contains(days.getFirst())),
+                () -> assertTrue(responsePhrase.contains(days.get(1))),
                 () -> assertFalse(objectResponse.getBoolean("end_session")),
                 () -> assertNull(getValue.apply(response, "session_state"))
         );
