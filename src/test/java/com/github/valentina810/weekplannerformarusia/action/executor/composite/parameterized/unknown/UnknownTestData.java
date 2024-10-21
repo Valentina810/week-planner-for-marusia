7e7fdc065abaaacaa184e941f557b3653a64f80d
valentinakolesnikova <valentinavasileva34@gmail.com>
2024-10-21 22:14:56 +0300

    feat: Упростить providerUnknownTest

--- src/test/java/com/github/valentina810/weekplannerformarusia/action/executor/composite/parameterized/unknown/UnknownTestData.java
+++ src/test/java/com/github/valentina810/weekplannerformarusia/action/executor/composite/parameterized/unknown/UnknownTestData.java
@@ -4,62 +4,28 @@ import org.junit.jupiter.params.provider.Arguments;
 
 import java.util.stream.Stream;
 
-import static com.github.valentina810.weekplannerformarusia.action.executor.composite.parameterized.unknown.ParameterForUnknownTest.*;
 import static org.junit.jupiter.params.provider.Arguments.of;
 
 public class UnknownTestData {
 
     static Stream<Arguments> providerUnknownTest() {
-
         return Stream.of(
-                of(builder()
-                        .phrase("PhraseUnknown")
-                        .build()),
-                of(builder()
-                        .phrase("")
-                        .build()),
-                of(builder()
-                        .phrase("сегодня")
-                        .build()),
-                of(builder()
-                        .phrase("завтра")
-                        .build()),
-                of(builder()
-                        .phrase("план")
-                        .build()),
-                of(builder()
-                        .phrase("понедельник")
-                        .build()),
-                of(builder()
-                        .phrase("вторник")
-                        .build()),
-                of(builder()
-                        .phrase("среда")
-                        .build()),
-                of(builder()
-                        .phrase("четверг")
-                        .build()),
-                of(builder()
-                        .phrase("пятница")
-                        .build()),
-                of(builder()
-                        .phrase("суббота")
-                        .build()),
-                of(builder()
-                        .phrase("воскресенье")
-                        .build()),
-                of(builder()
-                        .phrase("часов")
-                        .build()),
-                of(builder()
-                        .phrase("минут")
-                        .build()),
-                of(builder()
-                        .phrase("часов минут")
-                        .build()),
-                of(builder()
-                        .phrase("понедельник вторник")
-                        .build())
+                of("PhraseUnknown"),
+                of(""),
+                of("сегодня"),
+                of("завтра"),
+                of("план"),
+                of("понедельник"),
+                of("вторник"),
+                of("среда"),
+                of("четверг"),
+                of("пятница"),
+                of("суббота"),
+                of("воскресенье"),
+                of("часов"),
+                of("минут"),
+                of("часов минут"),
+                of("понедельник вторник")
         );
     }
 }
